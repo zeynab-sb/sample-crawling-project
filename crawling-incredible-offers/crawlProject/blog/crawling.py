920d40dec9af7dcf7706b91546aaf01993d970d9
Zeynab Sobhani <zeynab.sobhani6294@gmail.com>
2021-07-06 13:27:39 +0430

    delete unused imports and variables

--- crawling-incredible-offers/crawlProject/blog/crawling.py
+++ crawling-incredible-offers/crawlProject/blog/crawling.py
@@ -1,11 +1,9 @@
 import lxml
 import re
-import numpy as np
 import pandas as pd
 from bs4 import BeautifulSoup
 from requests import get
 
-url = "https://www.digikala.com/incredible-offers/"
 class Products(object):
 
     def __init__(self, url):
